938b603811b5cb7e21e00bdc0fa6500747b38497
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-12-27 20:01:32 +0700

    Tes assigment logical and dan logical or

---
+++ src/appjs_nestedif.js
@@ -0,0 +1,35 @@
+// validasi sederhana untuk password
+const password = '123 467';
+
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+        console.log('password sudah benar', password);
+    } else {
+        console.log('password tidak boleh ada spasi');
+    }
+} else {
+    console.log('Password terlalu pendek');
+}
+
+// dengan cara yang lebih ringkas
+if (password.length >= 6 && password.indexOf(' ') === -1) {
+    console.log('Kata sandi sudah benar', password);
+} else {
+    console.log('Password terlalu pendek.', 'Password tidak boleh ada spasi.');
+}
+
+// TRUTH AND FALSY VALUE 0, false, "", nan, undefined, null
+const isValueTruthFalsy = 'hello apa kabar';
+
+if (isValueTruthFalsy) {
+    console.log('Nilai termasuk TRUTHY');
+} else {
+    console.log('Nilai termasuk FALSY');
+}
+
+const bilanganBanding = 11;
+if (bilanganBanding >= 1 && bilanganBanding <= 10) {
+    console.log('bilangan ada di dalam rentang', bilanganBanding);
+} else {
+    console.log('bilangan tidak ada di dalam rentang', bilanganBanding);
+}
