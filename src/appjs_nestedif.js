c988fc18f904380be49e36dc66706d2910b74215
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-12-28 10:02:02 +0700

    testing: Logical operator OR and AND

--- src/appjs_nestedif.js
+++ src/appjs_nestedif.js
@@ -33,3 +33,22 @@ if (bilanganBanding >= 1 && bilanganBanding <= 10) {
 } else {
     console.log('bilangan tidak ada di dalam rentang', bilanganBanding);
 }
+
+const umurPenonton = 65;
+if (umurPenonton < 7 || umurPenonton >= 60) {
+    console.log('Anda bisa naik bus gratis', umurPenonton);
+} else {
+    console.log(
+        `Anda harus membayar tiket bus karena umur anda ${umurPenonton}`
+    );
+}
+
+const warnaPilih = 'biru';
+
+if (
+    warnaPilih === 'hijau' ||
+    warnaPilih === 'biru' ||
+    warnaPilih === 'orange'
+) {
+    console.log('PILIHAN ANDA BERSELERA TINGGI', warnaPilih);
+}
