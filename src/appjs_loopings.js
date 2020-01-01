c4fc98a0d9f1c08fc9990e9152063c27f3db1252
GulajavaMinistudio <gulajava.mini@gmail.com>
2020-01-01 12:45:30 +0700

    testing: Looping object dan array

---
+++ src/appjs_loopings.js
@@ -0,0 +1,8 @@
+const siswaNilaiNama = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Magnetic', value: 13 },
+    { name: 'Zeros', value: 37 },
+];
