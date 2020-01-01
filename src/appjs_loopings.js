67dfd4d02b70be3d95b3ae60cc2ea457056510f1
GulajavaMinistudio <gulajava.mini@gmail.com>
2020-01-01 17:50:49 +0700

    testing: looping array

--- src/appjs_loopings.js
+++ src/appjs_loopings.js
@@ -6,3 +6,28 @@ const siswaNilaiNama = [
     { name: 'Magnetic', value: 13 },
     { name: 'Zeros', value: 37 },
 ];
+
+const panjangData = siswaNilaiNama.length;
+for (let i = 0; i < panjangData; i += 1) {
+    const objectSiswa = siswaNilaiNama[i];
+    console.log('NAMA SISWA ', objectSiswa.name);
+    console.log('NILAI SISWA ', objectSiswa.value);
+}
+
+// PERULANGAN FOR LOOP
+for (let i = 0; i < 10; i += 1) {
+    console.log('PERULANGAN KE ', i);
+}
+
+for (let i = 1; i <= 10; i += 1) {
+    console.log('PERULANGAN 1 SAMPAI 10 : ', i);
+}
+
+for (let i = 1; i <= 10; i += 1) {
+    const perkalian = i * i;
+    console.log(`${i} x ${i} = ${perkalian}`);
+}
+
+for (let i = 50; i >= 0; i -= 10) {
+    console.log('LOOP MUNDUR ', i);
+}
