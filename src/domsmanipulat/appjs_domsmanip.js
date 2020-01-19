d6460b0caeaeb4a647b52dda16c3e184d83a7e94
GulajavaMinistudio <gulajava.mini@gmail.com>
2020-01-19 13:45:23 +0700

    testing: DOM manipulation

---
+++ src/domsmanipulat/appjs_domsmanip.js
@@ -0,0 +1,8 @@
+// INNERTEXT DOM HTML
+// Ambil data teks di dalam DOM Selector dengan Inner Text
+const selectorH1text = document.querySelector('h1');
+console.log('INNER TEXT SELECTOR H1', selectorH1text.innerHTML);
+
+// mengambil semua teks di dalam ul
+const selectorUl = document.querySelector('ul');
+console.log('INNER TEXT SELECTOR UL', selectorUl.innerText);
