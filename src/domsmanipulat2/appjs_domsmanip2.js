c71d8925bfea61e3520412ab6054b2c7b83637f6
GulajavaMinistudio <gulajava.mini@gmail.com>
2020-01-20 10:10:36 +0700

    testing: manipulasi dom dengan src img dan a href

--- src/domsmanipulat2/appjs_domsmanip2.js
+++ src/domsmanipulat2/appjs_domsmanip2.js
@@ -9,9 +9,16 @@ for (let i = 0; i < inputListArray.length; i += 1) {
 const inputValue = document.querySelector(
     'form input.inputdata[name="teksinput"]',
 );
+
+const inputPasswordEl = document.querySelector(
+    'form input[name="passwordinput"]',
+);
+
 setTimeout(() => {
     console.log('Input value username berubah setelah beberapa saat');
     inputValue.value = 'Input value simsalabim muncul';
+
+    inputPasswordEl.placeholder = 'Silahkan isi kata sandi anda';
 }, 3000);
 
 // CHECKBOX Input memiliki cara sendiri dalam mengambil nilai value
@@ -21,8 +28,8 @@ console.log('LIHAT VALUE CHECKBOX', checkbox.checked, checkbox);
 const arrayInputRange = document.querySelectorAll('form input[type="range"]');
 const inputRangePilih = arrayInputRange[0];
 setTimeout(() => {
-    inputRangePilih.value = 250;
-    console.log('INPUT RANGE VALUE', inputRangePilih.value);
+    inputRangePilih.value = 450;
+    console.log('INPUT RANGE VALUE BERUBAH', inputRangePilih.value);
 }, 2000);
 
 // contoh ubah nilai checkbox
@@ -30,3 +37,22 @@ setTimeout(() => {
     checkbox.checked = false;
     console.log('Nilai checkbox berubah lagi');
 }, 2000);
+
+// QUERYSELECTOR HREF DARI ALINK
+const elementAref = document.querySelector('a');
+const urlElement = elementAref.href;
+console.log('Url element href', urlElement);
+
+setTimeout(() => {
+    console.log('Href berubah');
+    elementAref.href =
+        'https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML';
+}, 3000);
+
+// GANTI GAMBAR DENGAN IMG SRC
+const gambarSelect = document.querySelector('img');
+
+setTimeout(() => {
+    console.log('Ganti gambar foto kucing');
+    gambarSelect.src = 'https://source.unsplash.com/9SWHIgu8A8k';
+}, 3000);
