498fd5eaa4943f81853c2a37a4df3dad75818154
GulajavaMinistudio <gulajava.mini@gmail.com>
2020-01-21 19:40:31 +0700

    testing: append child, insert before, insert adjacent, append, prepend dom element html

--- src/domsmanipulat5/appjs_doms5.js
+++ src/domsmanipulat5/appjs_doms5.js
@@ -37,3 +37,111 @@ console.log('A href kucing element', linkKucingElement);
 
 const paragrafDua = document.querySelector('p');
 paragrafDua.appendChild(linkKucingElement);
+
+// INSERTBEFORE Memasukkan element ke awal dom element di dalam parent
+// Insert before membutuhkan parent element, dan child element yang akan dipakai patokan element sebelumnya insert element before
+const parentUlElement = document.querySelector('div #todolist');
+console.log(parentUlElement);
+
+const liElementBaru = document.createElement('li');
+const liElementInsertTengah = document.createElement('li');
+
+liElementBaru.innerText = 'Element Baru Olahraga pagi dulu';
+liElementBaru.classList.add('todo');
+
+liElementInsertTengah.innerText = 'Element Makan Pagi di Tengah';
+liElementInsertTengah.classList.add('todo');
+
+console.log(liElementBaru);
+// menambahkan di bagian belakang
+// parentUlElement.appendChild(liElementBaru);
+
+// menambahkan di bagian depan
+// let insertedNode = parentNode.insertBefore(newNode, referenceNode)
+const elementLiPertama = document.querySelector('div #todolist li.todo');
+
+const elementTodoTerakhir = document.querySelectorAll(
+    'div #todolist li.todo',
+)[2];
+
+console.log(elementLiPertama);
+console.log(elementTodoTerakhir);
+
+parentUlElement.insertBefore(liElementBaru, elementLiPertama);
+parentUlElement.insertBefore(liElementInsertTengah, elementTodoTerakhir);
+
+// INSERTADJACENT CARA LAIN INSERT ELEMENT PADA INDEKS ELEMENT TERTENTU
+const elementMiring = document.createElement('i');
+const elementMiringAkhir = document.createElement('i');
+const elementMiringTengahAwal = document.createElement('i');
+const elementMiringTengahAkhir = document.createElement('i');
+
+elementMiring.innerText = 'Contoh Teks Miring Awal';
+elementMiring.style.fontWeight = 'bold';
+
+elementMiringAkhir.innerText = 'Contoh Teks Miring Akhir';
+elementMiringAkhir.style.fontWeight = 'bold';
+
+elementMiringTengahAwal.innerText = 'Contoh Teks Setelah Awal-';
+elementMiringTengahAwal.style.fontWeight = 'bold';
+
+elementMiringTengahAkhir.innerText = '-Contoh Teks Miring Sebelum Akhir';
+elementMiringTengahAkhir.style.fontWeight = 'bold';
+
+const paragrafKedua = document.querySelector('#paragrafdua');
+
+paragrafKedua.insertAdjacentElement('beforebegin', elementMiring);
+paragrafKedua.insertAdjacentElement('afterend', elementMiringAkhir);
+
+paragrafKedua.insertAdjacentElement('afterbegin', elementMiringTengahAwal);
+paragrafKedua.insertAdjacentElement('beforeend', elementMiringTengahAkhir);
+
+// INSERTADJACENTELEMENT LATIHAN LAGI
+const ulElementAdjacent = document.querySelector('#ul_adjacent ul');
+console.log(ulElementAdjacent);
+
+const liInsertAwalan = document.createElement('li');
+const liInsertSetelahAwal = document.createElement('li');
+const liInsertSebelumAkhir = document.createElement('li');
+const liInsertAkhiran = document.createElement('li');
+
+liInsertAwalan.innerText = 'Baris Masuk Sebelum Element Target Awal';
+liInsertSetelahAwal.innerText = 'Baris Masuk Setelah Awal';
+liInsertSebelumAkhir.innerText = 'Baris Masuk Sebelum Akhir';
+liInsertAkhiran.innerText = 'Baris Masuk Setelah Element Target Akhir';
+
+ulElementAdjacent.insertAdjacentElement('beforebegin', liInsertAwalan);
+ulElementAdjacent.insertAdjacentElement('afterbegin', liInsertSetelahAwal);
+ulElementAdjacent.insertAdjacentElement('beforeend', liInsertSebelumAkhir);
+ulElementAdjacent.insertAdjacentElement('afterend', liInsertAkhiran);
+
+// APPEND Insert element di akhir child node di dalam parent
+// PREPEND Insert element di awal child node di dalam parent
+// APPENDPREPEND bisa insert banyak element sekaligus
+const liElementAkhirAppend = document.createElement('li');
+liElementAkhirAppend.innerText = 'Element Akhir: Append Li';
+liElementAkhirAppend.style.fontStyle = 'italic';
+liElementAkhirAppend.style.fontWeight = 'bold';
+
+const liElementAwalPrepend = document.createElement('li');
+liElementAwalPrepend.innerText = 'Element Awal: Append Li';
+liElementAwalPrepend.style.fontStyle = 'italic';
+liElementAwalPrepend.style.fontWeight = 'bold';
+
+const spanElementAwalPrepend = document.createElement('span');
+spanElementAwalPrepend.innerText = 'Element Awal: Prepend Span';
+spanElementAwalPrepend.style.fontStyle = 'italic';
+spanElementAwalPrepend.style.fontWeight = 'bold';
+
+const paragrafAppendPrepend = document.querySelector('#p_appendprepends');
+paragrafAppendPrepend.append(liElementAkhirAppend, spanElementAwalPrepend);
+// paragrafAppendPrepend.insertAdjacentElement(
+//     'afterbegin',
+//     spanElementAwalPrepend,
+// );
+paragrafAppendPrepend.prepend(spanElementAwalPrepend);
+
+const ulAppendPrepend = document.querySelector('#ul_appendprepend ul');
+ulAppendPrepend.append(liElementAkhirAppend, spanElementAwalPrepend);
+
+ulAppendPrepend.prepend(spanElementAwalPrepend, liElementAwalPrepend);
