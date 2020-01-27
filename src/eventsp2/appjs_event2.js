eb4afeb26729cdcccb2255e9dc8032a1b3120249
GulajavaMinistudio <gulajava.mini@gmail.com>
2020-01-27 14:57:03 +0700

    quiz: keycode event dan baca input untuk todolist

--- src/eventsp2/appjs_event2.js
+++ src/eventsp2/appjs_event2.js
@@ -40,9 +40,60 @@ for (let i = 0; i < panjangArray; i += 1) {
     kotakElement.addEventListener('click', cetakWarnaKotak);
 }
 
+// KEYPRESS KEYUP KEYDOWN
+// Event listener ketika keyboard telah ditekan, keyboard dipencet, dan ketika keyboard dipencet
+
 // KEYPRESS EVENT OBJECT
 const inputKeyPress = document.querySelector('div #inputkey');
 inputKeyPress.addEventListener('keypress', event => {
     console.log(event);
     console.log('Key Ditekan', event.key, event.keyCode);
 });
+
+// KEYDOWN EVENT LISTENER
+const inputKeydown = document.querySelector('#input_keydown');
+inputKeydown.addEventListener('keydown', event => {
+    console.log('KEYDOWN EVENT ', event);
+});
+
+// KEYUP EVENT LISTENER
+const inputKeyup = document.querySelector('#input_keyup');
+inputKeyup.addEventListener('keyup', event => {
+    console.log('KEYUP EVENT ', event);
+});
+
+// KEYPRESS CONTOH
+// Contoh Kasus penggunaan Key Press
+const inputTambahMakanan = document.querySelector('div #input_makanan');
+const listDaftarMakanan = document.querySelector('div #daftar_item');
+
+const tambahMakanan = event => {
+    const elementTarget = event.target;
+    const stringNamaMakanan = elementTarget.value;
+
+    if (stringNamaMakanan && stringNamaMakanan.length > 1) {
+        const liItem = document.createElement('li');
+        liItem.innerText = stringNamaMakanan;
+        listDaftarMakanan.appendChild(liItem);
+
+        // Kosongkan nilai input
+        elementTarget.value = '';
+    }
+};
+
+// Ambil aksi Enter dan jalankan ambil nilai value dari input
+const initListenerInput = () => {
+    inputTambahMakanan.addEventListener('keypress', event => {
+        const keyCodeEvent = event.keyCode;
+        const stringKeyCode = event.key;
+
+        if (keyCodeEvent === 13 || stringKeyCode === 'Enter') {
+            // tambah ke dalam daftar
+            // jika pake arrow function, tidak bisa pake this.
+            // console.log(this.value);
+            tambahMakanan(event);
+        }
+    });
+};
+
+initListenerInput();
